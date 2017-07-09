98a656b4b77f70829e9620c3e9a494117af1418c
squeemishly <squeemishly@gmail.com>
2017-07-09 15:20:12 -0600

    user can now see a list of orgs they belong to

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -1,6 +1,14 @@
 class GithubService
+  # def initialize
+  #   @conn = Faraday.new(url: "https://api.github.com/users")
+  # end
+
   def self.create_list(type, user)
-    response = Faraday.get("https://api.github.com/users/#{user.screen_name}/#{type}")
+    # response = conn.get "/#{user.screen_name}/#{type}"
+    response = Faraday.get "https://api.github.com/users/#{user.screen_name}/#{type}"
     JSON.parse(response.body, symbolize_names: true)
   end
+
+  # private
+  # attr_reader :conn
 end
